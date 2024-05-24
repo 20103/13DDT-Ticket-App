a63b14a4308ab52539ff436e42541bbea0ed30af
20103 <hamcheesesnadwich@gmail.com>
2024-05-24 13:18:28 +1200

    initialise login database, insert dummy values

---
+++ login_db.py
@@ -0,0 +1,27 @@
+import sqlite3
+connection = sqlite3.connect("logins.db")
+cursor = connection.cursor()
+
+#Similarly to ticket_db, initialises the db if it doesn't already exist and inserts dummy values for testing.
+create_table_command = """CREATE TABLE IF NOT EXISTS user_logins (username text, password text)"""
+
+#Inserts rows of table into db.
+cursor.execute(create_table_command)
+
+
+#Dummy values.
+#Also, consider linking logins with ticket. Either as two separate dbs, or a table in one?
+logins = [
+    ("Admin", "1234"), 
+    ("User1", "password"),
+    ("catlover", "ilovecats")]
+
+#Prints out values from db.
+cursor.executemany("insert into user_logins values (?,?)" , logins)
+
+for row in cursor.execute("select * from user_logins"):
+    print(row)
+
+#Save and close the connection.
+connection.commit()
+connection.close()
