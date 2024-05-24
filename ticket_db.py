ca73af3c915af87deaf250e7e5f2c07d4a6a38e3
20103 <hamcheesesnadwich@gmail.com>
2024-05-24 13:20:30 +1200

    added comments for clarification

--- ticket_db.py
+++ ticket_db.py
@@ -2,23 +2,26 @@ import sqlite3
 connection = sqlite3.connect("tickets.db")
 cursor = connection.cursor()
 
-#CREATE a table named customer
-
+#Similarly to login_db, initialises the db if it doesn't already exist and inserts dummy values for testing.
 create_table_command = """CREATE TABLE IF NOT EXISTS ticket_details (venue text, time_created text, wait_time text)"""
 
+#Executes the SQL code.
 cursor.execute(create_table_command)
 
+#Dummy values.
 tickets = [
     ("McDonalds", "9:15pm", "20 Minutes"), 
     ("Burger King", "1 Hour ago", "30 Seconds"), 
     ("Macleans College", "2 Days ago", "Now"), 
     ("Countdown", "14/05/2024 09:57 PM", "1 Hour")]
 
-
+#Inserts rows of table into db.
 cursor.executemany("insert into ticket_details values (?,?,?)" , tickets)
 
+#Prints out values from db
 for row in cursor.execute("select * from ticket_details"):
     print(row)
 
+#Save and close the connection.
 connection.commit()
-cursor.close()
+connection.close()
