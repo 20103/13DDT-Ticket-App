b3f7527775cc2bc5a1c55cf3a916704116d71bb0
20103 <hamcheesesnadwich@gmail.com>
2024-06-27 13:24:41 +1200

    Cleaner code for checking valid login, login button

--- signuppage.py
+++ signuppage.py
@@ -8,30 +8,49 @@ class SignUpPage:
         root.title("Sign-Up")
         root.geometry("400x600")
 
+        def Login():
+            root.destroy()
+            subprocess.run(["python", "loginpage.py"])
+        
+        #Function which begins the process of validating signup information.
         def ValidateSignup(user, pass1, pass2):
+
+            if not ValidString(user):
+                #If username is empty
+                print("Please enter a username")
+                return [False, "Please enter a username"]
+
+            if pass1 != pass2:
+                #If passwords do not match, stays Falsey
+                print("Passwords do not match")
+                return [False, "Passwords do not match"]
+            
+            if not ValidString(pass1):
+                print("Password must not be empty.")
+                return [False, "Password must not be empty"] 
+
             result = [False, "No message provided"]
-            if pass1 == pass2:     
-                print("Passwords match")
-                connection = sqlite3.connect("logins.db")
-                cursor = connection.cursor()
-                #Checks every row in login_details for a match.
-                try:
-                    cursor.execute("SELECT 1 FROM user_logins where username =?", (user,))
-                    if cursor.fetchone() is not None:
-                        print("Username already exists")
-                        result[1] = "Username already exists"
-                    else:
-                        print("No duplicates found")
-                        result = [True, "Account created"]
-                except sqlite3.Error as e:
-                    print(f"Database could not be fetched: {e}")
-                    result[1] = f"Database error: {e}"
-                finally:
-                    connection.close()
-            else:
-                print("Passwords do not match.")
-                result[1] = "Passwords do not match"
-            return result
+                
+            connection = sqlite3.connect("logins.db")
+            cursor = connection.cursor()
+            #Checks every row in login_details for a match.
+            try:
+                cursor.execute("SELECT 1 FROM user_logins where username =?", (user,))
+                if cursor.fetchone() is not None:
+                    #Account already exists, stays Falsey
+                    print("Username already exists")
+                    return [False, "Password must not be empty"] 
+                else:
+                    #Account does not exist, becomes Truthy
+                    print("No duplicates found")
+                    return [True, "Account created"]
+            except sqlite3.Error as e:
+                #Database encountered an error, stays Falsey
+                print(f"Database could not be fetched: {e}")
+                result[1] = f"Database error: {e}"
+            finally:
+                #Once all operations are completed, close the connection.
+                connection.close()
                 
         def CreateAccount(username, password1, password2):
             feedback_widget = self.signup_feedback
@@ -39,25 +58,32 @@ class SignUpPage:
             Feedback = ValidateSignup(username, password1, password2)
             Success, Msg = Feedback[0], Feedback[1]
             if Success:
+               #ValidateSignup() function returns true, continue with account creation
                connection = sqlite3.connect("logins.db")
                cursor = connection.cursor()
                try:
+                    #Organises account data into a table, which is then inserted into the user_logins table.
                     user_login = [username, password1]
-                    #ERROR: Can't insert data
                     cursor.executemany("insert into user_logins values (?, ?)", (user_login,))
                     print("Created account")
                except sqlite3.Error as e:
                     print(f"Database could not be fetched: {e}")
                     Msg = f"Database error: {e}"
                finally:
+                    #Commit all changes and close the connection.
                     connection.commit()
                     connection.close()
 
                print("Logging in")
+               #Destroys window and opens homepage.py
                root.destroy()
                subprocess.run(["python", "homepage.py"])  
                print("Home")
+            #Message changes depending on feedback given.
             feedback_widget.config(text=Msg)    
+
+        def ValidString(str):
+            return not str.isspace() and len(str) > 0
 
         #Configure root
         root.grid_rowconfigure(0, weight=1)
@@ -98,6 +124,9 @@ class SignUpPage:
 
         self.signup_button = ttk.Button(self.credentials_frame, style="Accent.TButton", text="Create Account", command=lambda: CreateAccount(username_submitted.get(), password_submitted.get(), password_confirmation_submitted.get()))
         self.signup_button.grid(row=4, column=0, sticky="nsew", padx=10, pady=10, columnspan=2)
+
+        self.login_button = ttk.Button(self.credentials_frame, text="Go back to login page", command=Login)
+        self.login_button.grid(row=5, column=0, columnspan=2)
 
 if __name__ == "__main__":
     root = tk.Tk()
